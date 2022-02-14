032145a81c90bca06e38e219676f250d2a201364
Markus Davis <85118308+MarkusDavis@users.noreply.github.com>
2022-02-14 01:21:29 -0500

    Delete .history directory

--- .history/src/constants/routes_20210819121622.js
+++
@@ -1,8 +0,0 @@
-export const DASHBOARD = ('/'|'/home');
-export const LOGIN = '/login';
-export const BOOK = '/books';
-export const SIGN_UP = '/signup';
-export const PROFILE = '/p/:username';
-export const EDITPROFILE = '/edit';
-export const NOT_FOUND = '/not-found';
-export const PROFILESKELETON = '/profile';
